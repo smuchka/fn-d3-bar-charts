42067cf89e25043efa2e7ef2336af0c40447a546
smuchka <smuchka.cherry@gmail.com>
2019-07-12 15:49:29 +0300

    fix all copy componenrts

--- src/app/statistic-chart/shared/bar-chart/bar-chart.module.ts
+++ src/app/statistic-chart/shared/bar-chart/bar-chart.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseD3ChartComponent } from './base-d3-chart.component';
 import { BarChartSimpleComponent } from './bar-chart-simple/bar-chart-simple.component';
+import { BarChartTimeScaleComponent } from './bar-chart-time-scale/bar-chart-time-scale.component';
 
 @NgModule({
   imports: [
     CommonModule,
   ],
   declarations: [
-    BarChartSimpleComponent,
     BaseD3ChartComponent,
+    BarChartSimpleComponent,
+    BarChartTimeScaleComponent,
   ],
   exports: [
     BarChartSimpleComponent,
+    BarChartTimeScaleComponent
   ],
 })
 export class BarChartModule {
