04e295bb3520bcef43fe28c4594065a2b023200a
smuchka <smuchka.cherry@gmail.com>
2019-07-12 18:12:52 +0300

    Make rect with round corners

--- src/app/statistic-chart/shared/bar-chart/bar-chart-time-scale/bar-chart-time-scale.component.ts
+++ src/app/statistic-chart/shared/bar-chart/bar-chart-time-scale/bar-chart-time-scale.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 import { BaseD3ChartComponent } from '../base-d3-chart.component';
 import { ItemData } from '../core/interfaces/item-data';
-import { startOfToday, endOfToday, differenceInHours } from 'date-fns'
+import { startOfToday, endOfToday, differenceInHours, addHours } from 'date-fns'
 import * as D3 from 'd3';
 
 @Component({
@@ -33,18 +33,22 @@ export class BarChartTimeScaleComponent extends BaseD3ChartComponent implements 
     // if data exist
 
     let barWidth = 20;
+    let radiusRectangle = 4;
     let startRange = startOfToday();
     let endRange = endOfToday();
-    let rangeEmptyData = (new Array(
-      differenceInHours(startRange, endRange)
-    )).fill(null)
 
-    console.warn(':', differenceInHours(startRange, endRange));
+    const countDays = differenceInHours(endRange, startRange) + 1;
+    let rangeEmptyData =
+      Array.from(Array(countDays), (el, index) => {
+        return addHours(startRange, index);
+      });
+
+    console.warn(':', differenceInHours(endRange, startRange));
 
     let x = D3.scaleTime()
       .domain([startRange, endRange])
       .range([this.margin.left, this.width - this.margin.right]);
-      x.ticks(D3.timeMinute.every(60));
+    x.ticks(D3.timeMinute.every(60));
 
     const inputMax = 999;
     const maxY = D3.max([
@@ -57,23 +61,32 @@ export class BarChartTimeScaleComponent extends BaseD3ChartComponent implements 
       .range([this.height - this.padding.bottom, this.padding.top])
       .nice();
 
+    console.warn(y(0), y(643));
+
+    // // // // // // // // //
+    // draw bar placeholder //
+    // // // // // // // // //
+    let placeholderBars = this.svg.append('g')
+      .selectAll('rect')
+      .data(rangeEmptyData);
+
+    placeholderBars
+      .join('rect')
+      .attr('x', d => x(d))
+      .attr('y', d => y(maxY))
+      .attr('rx', d => radiusRectangle)
+      .attr('ry', d => radiusRectangle)
+      .attr('height', d => y(0) - y(maxY))
+      .attr('width', barWidth)
+      .style('fill', '#F2F5FA')
+      .style('radius', '4px');
+
+    // draw bar
     let bars = this.svg.append('g')
       .attr('fill', 'steelblue')
       .selectAll('rect')
       .data(this.items);
 
-    console.warn(y(0), y(643));
-
-    // draw bar placeholder
-    bars
-      .join('rect')
-      .attr('x', d => x(d.data.date))
-      .attr('y', d => y(maxY))
-      .attr('height', d => y(0) - y(maxY))
-      .attr('width', barWidth)
-      .style('fill', 'yellow');
-
-    // draw bar
     bars
       .join('rect')
       .attr('x', d => x(d.identity))
