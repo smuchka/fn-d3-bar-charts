bc94a9be867b743e68b3797bdff6bd8bc6978fae
smuchka <smuchka.cherry@gmail.com>
2019-07-18 16:29:48 +0300

    Tmp reserv!

--- src/app/statistic-chart/shared/bar-chart/bar-chart-time-scale/bar-chart-time-scale.component.ts
+++ src/app/statistic-chart/shared/bar-chart/bar-chart-time-scale/bar-chart-time-scale.component.ts
@@ -20,7 +20,6 @@ const DirectionRight: DirectionActiveChange = 1;
 @Component({
   selector: 'fn-bar-chart-time-scale',
   template: `<!--d3 create template itself-->
-  <button (click)="onPositionZero()">to (0,0)</button>
   <button (click)="onClickToPrevActive()">︎←</button>
   <button (click)="onClickToNextActive()">→</button>
   `,
@@ -75,201 +74,65 @@ export class BarChartTimeScaleComponent extends BaseD3ChartComponent implements 
 
     // must exist data !!!!
     this.initActiveDate();
-    this.createXScale();
-    this.createYScale();
-    this.createZoom();
+    // this.activeDate = addHours(startOfToday(), random(1, 23));
+    // this.activeDate = addHours(startOfToday(), 10);
+
+    this.initXScale();
+    this.initYScale();
+    this.initZoom();
 
     // process drowing
     this.svg.selectAll().remove();
-    this.drawBottomAxis();
+    // this.drawBottomAxis();
     this.groupPlaceholderBars = this.svg.append('g').attr('class', 'placeholder');
     this.groupDataBars = this.svg.append('g').attr('class', 'bar');
+
+    this.showActiveBarOnCenterViewport();
     this.updateChart();
 
-    // 1. show active date in viewport
-    // 2. emit event - painning ended left/right! => upload more data ...
-    // 3. !!!! navigation nex to balance better
-    // 4. не упускать из виду активный
+    // Test: Dynamic change active date work fine!!!
+    //
+    // const timerId = setInterval(() => {
+    //   this.activeDate = addHours(startOfToday(), random(1, 23));
+    //   console.log(this.activeDate);
+    //   this.showActiveBarOnCenterViewport();
+    //   this.updateChart();
+    // }, 4000);
+
+    // setTimeout(function () {
+    //   clearInterval(timerId);
+    // }, 15000)
+
+    // TODO:
+    // - emit event - painning ended left/right! => upload more data ...
+    // - navigation on nexx/prev active
+    // - не упускать из виду активный
+
+    this.svg
+      .on("click", this.onSvgClick.bind(this));
   }
 
   public ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngChnage');
+    
     if (changes.items && changes.items.firstChange) {
       return;
     }
 
     if (changes.items && changes.items.currentValue) {
+
+      const isChanged: boolean = this.updateMaxChartValue()
+      if (isChanged) {
+        this.initYScale();
+      }
+
+      console.log('ngChnage items');
       this.updateChart();
     }
   }
 
-  protected initActiveDate(): void {
-    const arr = this.items.filter(d => d.value > 0)
-    const dateMax: Date | null = arr.length
-      ? D3.max(arr, d => d.identity)
-      : null;
-    const now = this.calcNowBarDate();
-    this.activeDate = dateMax
-      ? differenceInSeconds(dateMax, now) < 0 ? dateMax : now
-      : now;
-  }
-
-  private createXScale(): void {
-    const [d1, d2] = this.viewportDateRange();
-
-    this.x = D3.scaleTime()
-      .domain(this.viewportDateRange())
-      .range([
-        this.margin.left + this.padding.left,
-        this.width - this.margin.right - this.padding.right,
-      ]);
-    this.x2 = this.x.copy();
-
-    // calc width of one bar
-    this.translateWidthOneBar = Math.abs(
-      this.x(d1) - this.x(this.calcNextBarDate(d1))
-    );
-  }
-
-  private createYScale(): void {
-    this.y = D3.scaleLinear()
-      .domain([0, this.maxValueFromChart])
-      .range([this.height - this.padding.bottom, this.padding.top])
-      .nice();
-  }
-
-  private createZoom(): void {
-    this.zoom = D3.zoom()
-      .scaleExtent([1, 1])
-      .on("zoom", this.onZoomed.bind(this))
-      .on("end", this.onZoomedEnd.bind(this));
-
-    this.svg.call(this.zoom)
-  }
-
-  private updateZoomByChunk(from: Date, to: Date): void {
-    this.zoom = this.zoom
-      .extent([
-        [this.margin.left + this.padding.left, 0],
-        [this.width - this.margin.right - this.padding.right, 0]
-      ])
-      .translateExtent([
-        [this.x(from), 0],
-        [this.x(to), this.height]
-      ])
-  }
-
-  private checkAndUpdateMaxValue(): boolean {
-    const oldValue = this.maxValueFromChart;
-
-    this.maxValueFromChart = D3.max([
-      this.initMaxValue,
-      D3.max(this.items, d => d.value),
-    ]);
-
-    return this.maxValueFromChart !== oldValue;
-  }
-
-  // handlers
-  // todo: check it
-  private svgTranslateX(x: number = 0, animationDuration: number = 750): void {
-    this.svg
-      .transition()
-      .duration(animationDuration)
-      // .call(
-      //   this.zoom.transform,
-      //   D3.zoomIdentity.translate(x, 0)
-      // );
-      .call(this.zoom.translateBy, x, 0);
-  }
-
-  // todo: check it
-  private translateBarInViewPort(date: Date): void {
-    const x = this.x(date);
-    this.svgTranslateX(x);
-  }
-
-  // todo: check it
-  public onPositionZero(): void {
-    this.svgTranslateX(0)
-  }
-
-  private canChangeActiveOn(dir: DirectionActiveChange): boolean {
-    if (!this.activeDate) {
-      return false;
-    }
-
-    const endDirectionDate: date = (
-      (dir === DirectionRight) ? this.items[this.items.length - 1] : this.items[0]
-    ).identity;
-    const diffDates: number = differenceInSeconds(endDirectionDate, this.activeDate);
-
-    return (dir === DirectionRight) ? diffDates > 0 : diffDates < 0;
-  }
-
-  // todo: check it
-  public onClickToPrevActive(): void {
-    if (!this.canChangeActiveOn(DirectionLeft)) return;
-
-    this.activeDate = this.calcPrevBarDate();
-    this.updateChart();
-    // this.moveX(this.translateWidthOneBar)
-  }
-
-  // todo: check it
-  public onClickToNextActive(): void {
-    if (!this.canChangeActiveOn(DirectionRight)) return;
-
-    this.activeDate = this.calcNextBarDate(this.activeDate);
-    this.updateChart();
-
-    console.log(
-      this.x(0),
-      this.x(this.width),
-      this.x(this.activeDate)
-    )
-    // this.moveX(this.translateWidthOneBar * -1)
-  }
-
-  // todo: check it
-  private moveX(count: number = 0, animationDuration: number = 100): void {
-
-    this.groupDataBars
-      .transition()
-      .duration(animationDuration)
-      .call(this.zoom.translateBy, count, 0);
-
-    this.groupPlaceholderBars
-      .transition()
-      .duration(animationDuration)
-      .call(this.zoom.translateBy, count, 0);
-  }
-
-  // todo: hours dependencies
-  protected viewportDateRange(): [Date, Date] {
-    const from: Date = this.items[0].identity;
-    return [from, addHours(from, this.countViewBars - 1)];
-  }
-
-  // todo: hours dependencies
-  protected calcNowBarDate(): Date {
-    const now = new Date();
-    now.setMinutes(0);
-    now.setSeconds(0);
-    now.setMilliseconds(0);
-    return now;
-  }
-
-  // todo: hours dependencies
-  protected calcNextBarDate(from: Date): Date {
-    return addHours(from, 1);
-  }
-
-  // todo: hours dependencies
-  protected calcPrevBarDate(): Date {
-    return addHours(this.activeDate, -1);
-  }
-
   private onZoomed(): void {
+    console.log('onZoomed');
 
     // recalc X Scale and redraw xAxis
     this.x = D3.event.transform.rescaleX(this.x2);
@@ -284,24 +147,49 @@ export class BarChartTimeScaleComponent extends BaseD3ChartComponent implements 
     this.groupDataBars.attr("transform", "translate(" + x + ",0)");
   }
 
-  // todo: check it
   private onZoomedEnd(): void {
-    // console.log(
-    //   'zoomEnd',
-    //   this,
-    //   D3.event
-    // )
+    console.log('zoomEnd', this, D3.event)
+
+    // TODO:
+    // detect here zoom/scroll ending and emit event outside
+    // use it for paggination -> upload prev chunk
+  }
+
+  private onBarClick(d: ItemData): void {
+  }
+
+  private onSvgClick(d): void {
+    // console.log('click svg - ', D3.zoomIdentity, format(this.activeDate, 'HH:mm'));
+  }
+
+  private updateMaxChartValue(): boolean {
+    const oldValue = this.maxValueFromChart;
+
+    this.maxValueFromChart = D3.max([
+      this.initMaxValue,
+      D3.max(this.items, d => d.value),
+    ]);
+
+    return this.maxValueFromChart !== oldValue;
+  }
+
+  private updateZoomOnChangeData(from: Date, to: Date): void {
+    this.zoom = this.zoom
+      .extent([
+        [this.margin.left + this.padding.left, 0],
+        [this.width - this.margin.right - this.padding.right, 0]
+      ])
+      .translateExtent([
+        [this.x(from), 0],
+        [this.x(to), this.height]
+      ])
   }
 
   private updateChart(): void {
 
-    if (this.checkAndUpdateMaxValue()) {
-      this.createYScale();
-    }
-
     // update potencial availabel scroll zone
     // (limited current X range dates)
-    this.updateZoomByChunk(
+    this.updateZoomOnChangeData(
       D3.min(this.items, d => d.identity),
       D3.max(this.items, d => d.identity),
     );
@@ -325,17 +213,130 @@ export class BarChartTimeScaleComponent extends BaseD3ChartComponent implements 
       .call(this.drawDataBar.bind(this))
   }
 
-  // todo: check it !!!!
-  private updateActiveBar(): void {
-    this.groupDataBars
-      .selectAll('rect')
-      .call(this.drawDataBar.bind(this))
-
-    this.groupPlaceholderBars
-      .selectAll('.label')
-      .data(this.items)
-      .call(this.drawBarLabel.bind(this))
+  private initActiveDate(): void {
+    const arr = this.items.filter(d => d.value > 0)
+    const dateMax: Date | null = arr.length
+      ? D3.max(arr, d => d.identity)
+      : null;
+    const now = this.calcNowBarDate();
+    this.activeDate = dateMax
+      ? differenceInSeconds(dateMax, now) < 0 ? dateMax : now
+      : now;
   }
+
+  private initXScale(): void {
+    const [d1, d2] = this.viewportDateRange();
+
+    this.x = D3.scaleTime()
+      .domain(this.viewportDateRange())
+      .rangeRound([
+        this.margin.left + this.padding.left,
+        this.width - this.margin.right - this.padding.right,
+      ])
+      .nice();
+    this.x2 = this.x.copy();
+
+    // calc width of one bar
+    this.translateWidthOneBar = Math.abs(
+      this.x(d1) - this.x(this.calcNextBarDate(d1))
+    );
+  }
+
+  private initYScale(): void {
+    this.y = D3.scaleLinear()
+      .domain([0, this.maxValueFromChart])
+      .range([this.height - this.padding.bottom, this.padding.top])
+      .nice();
+  }
+
+  private initZoom(): void {
+    this.zoom = D3.zoom()
+      .scaleExtent([1, 1])
+      .on("zoom", this.onZoomed.bind(this))
+      .on("end", this.onZoomedEnd.bind(this));
+
+    this.svg.call(this.zoom)
+  }
+
+  private showActiveBarOnCenterViewport(): void {
+    if (!this.activeDate) {
+      return;
+    }
+
+    let [initialX, initialY] = [this.x(this.activeDate), 0];
+
+    const layout = this.svg
+      .transition()
+      .duration(0)
+
+    // - create new transform and apply it
+    // const newTransform = D3.zoomIdentity.translate(-initialX, 0);
+    // layout.call(this.zoom.transform, newTransform);
+
+    // - BETTER!
+    // - need add offset
+    // layout.call(this.zoom.translateBy, -initialX, initialY);
+
+    // - on zoom function call method - translateTo
+    layout.call(this.zoom.translateTo, initialX, initialY)
+  }
+
+  private canChangeActiveOn(dir: DirectionActiveChange): boolean {
+    if (!this.activeDate) {
+      return false;
+    }
+
+    const endDirectionDate: date = (
+      (dir === DirectionRight) ? this.items[this.items.length - 1] : this.items[0]
+    ).identity;
+    const diffDates: number = differenceInSeconds(endDirectionDate, this.activeDate);
+
+    return (dir === DirectionRight) ? diffDates > 0 : diffDates < 0;
+  }
+
+  public onClickToPrevActive(): void {
+    if (!this.canChangeActiveOn(DirectionLeft)) return;
+
+    this.activeDate = this.calcPrevBarDate();
+    this.showActiveBarOnCenterViewport();
+    this.updateChart();
+  }
+
+  public onClickToNextActive(): void {
+    if (!this.canChangeActiveOn(DirectionRight)) return;
+
+    this.activeDate = this.calcNextBarDate(this.activeDate);
+    this.showActiveBarOnCenterViewport();
+    this.updateChart();
+  }
+
+  // // // // // // // // // // // // 
+  // TODO: need overload in child - hours | days | weeks
+  // // // // // // // // // // // // 
+
+  protected viewportDateRange(): [Date, Date] {
+    const from: Date = this.items[0].identity;
+    return [from, addHours(from, this.countViewBars - 1)];
+  }
+
+  protected calcNowBarDate(): Date {
+    const now = new Date();
+    now.setMinutes(0);
+    now.setSeconds(0);
+    now.setMilliseconds(0);
+    return now;
+  }
+
+  protected calcNextBarDate(from: Date): Date {
+    return addHours(from, 1);
+  }
+
+  protected calcPrevBarDate(): Date {
+    return addHours(this.activeDate, -1);
+  }
+
+  // // // // // // // // // // // // 
+  // // // // // // // // // // // //
 
   private drawDataBar(selection: any): void {
     this
@@ -350,16 +351,11 @@ export class BarChartTimeScaleComponent extends BaseD3ChartComponent implements 
       .attr('y', d => this.y(this.maxValueFromChart))
       .attr('height', d => this.y(0) - this.y(this.maxValueFromChart))
       .attr('class', 'bar placeholder')
-      .on("click", function (e) {
-        var coords = D3.mouse(this);
-
-        console.log(
-          'click',
-          coords,
-          D3.zoomTransform(this),
-          e
-        );
-      });
+    // .on("click", this.onBarClick.bind(this));
+    // .on("click", (e) => {
+    //   D3.zoomTransform();
+    //   console.log(e, this);
+    // })
   }
 
   private drawBarPrimitive(selection: Selection, color: string): Selection {
@@ -372,6 +368,7 @@ export class BarChartTimeScaleComponent extends BaseD3ChartComponent implements 
       .attr('rx', d => this.radiusRectangle)
       .attr('ry', d => this.radiusRectangle)
       .attr('class', 'bar')
+    // .on("click", this.onBarClick.bind(this));
   }
 
   private drawBarLabel(selection: any): void {
@@ -416,4 +413,8 @@ export class BarChartTimeScaleComponent extends BaseD3ChartComponent implements 
       .attr("transform", "translate(0," + (positionOnY) + ")")
       .call(this.xAxis);
   }
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (+max - +min)) + +min;
 }
